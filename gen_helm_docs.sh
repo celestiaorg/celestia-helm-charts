3dd1416b3c1304350ab1fd091392bb7204c539ee
Jose Ramon Mañes <jose@celestia.org>
2024-06-07 09:15:21 +0200

    feat: update script
    Signed-off-by: Jose Ramon Mañes <jose@celestia.org>

--- gen_helm_docs.sh
+++ gen_helm_docs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for i in $(ls charts|grep celestia); do
+for i in $(ls charts|grep -vE "template|README.md"); do
     cd charts/$i
+    ls -ltar
     echo "Generating docs for chart: [$i]"
     docker run --rm --volume "$(pwd):/helm-docs" -u $(id -u) jnorwood/helm-docs:latest
     # go to git repo root
